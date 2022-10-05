4858a1dc334f62ddc278d04107659f477339a2d2
sistemas <sistemas@cottucumano.com.ar>
2022-10-05 10:04:41 -0300

    Loading.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useRef, useState }  from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { Router,Route, Routes } from 'react-router-dom';
+
+import { getPer } from './Services/Persona';
+import Loading from './components/Loading';
+import axios from 'axios';
+
 import Card from 'react-bootstrap/Card';
 import { Alert, Button, ListGroup, Row, Table } from 'react-bootstrap';
-
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { getPer } from './Services/Persona';
-import axios from 'axios';
+import Nav from 'react-bootstrap/Nav';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
 
   const inputFile = useRef(null) 
   const [per,setPer] = useState({})
-  const [msj,setMsj] = useState('')
+  const [msj,setMsj] = useState('Agrega tu foto haciendo click en la imagen')
   const [estado,setEstado] = useState('')
+  const [imgDefault, setImgDefault] = useState(true)
 
   const [file, setFile] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
+  const [cargando, setCargando] = useState(true)
 
   const onButtonClick = () => {
   //  inputFile.current.click();
@@ -32,7 +37,7 @@ function App() {
     const objectURL = URL.createObjectURL(event.target.files[0])
     //setPreviewImage(objectURL)
     console.log(event.target.files[0])
-    handleSubmit(event)
+    //handleSubmit(event)
   }
 
   const handleSubmit = (event) => {
@@ -69,17 +74,13 @@ function App() {
   }
 
   useEffect(()=>{
-    getPer(1129).then(response=>{
+    //setCargando(true)
+    getPer(1010).then(response=>{
       
-      if(response.fchBaj === null){
-        setPer(response)
-        setEstado('Activo')
-        //setPreviewImage(response.picture)
-      }else{
-        console.log('baja de socio')
-        setPer([])
-        setEstado('Inactivo')
-      }
+      setPer(response)
+      if(response.picture != null){setImgDefault(false)}
+      response.fchBaj === null ? setEstado('Activo') : setEstado('Inactivo')
+      setCargando(false)
       
     }).catch(err=>{
         setMsj(err.message)
@@ -97,8 +98,30 @@ function App() {
            : `http://localhost:8080/api/files/${per.picture}`
   
       setPreviewImage(img)
-           
+      
   },[per,previewImage])
+
+  useEffect(() => {
+    if(!!file){
+      const data = new FormData();
+      data.append('file', file);
+
+      axios.post(`http://localhost:8080/api/personas/${per.id}`, data, { 
+        headers: {
+          "Content-Type": "multipart/form-data",
+        }
+      }).then(res => { // then print response status
+          console.log(res)
+          per.picture = res.data // VER
+          setPreviewImage(res.data)
+          setImgDefault(false)
+      }).catch((error) => {
+          console.error('Error:', error);
+      });
+    }
+    
+  }, [file])
+  
 
   return (
     // <Router>
@@ -135,68 +158,69 @@ function App() {
         </Navbar>
       </Card.Header>
 
-      {/* <Card border="info" className="text-center m-1">
-        <Card.Body>
-          <Card.Text>
-              Agrega tu foto haciendo click en la imagen
-          </Card.Text>
-        </Card.Body>
-      </Card> */}
 
-      <Alert key={'info'} variant={'info'} className="text-center m-1">
-          <strong>Agrega tu foto haciendo click en la imagen</strong>
-        </Alert>
+      {cargando 
+        ? <Loading/> 
+        : 
+        <>
+          { estado === 'Activo'
+            ? imgDefault && <Alert key={'info'} variant={'info'} className="text-center m-1">
+                <strong>Agrega tu foto haciendo click en la imagen</strong>
+              </Alert>
+            : <Alert key={'danger'} variant={'danger'} className="text-center m-1">
+                Llamá al <strong>381422830</strong> para regularizar tu situación
+              </Alert>
+          }
 
-      <Card.Body className="text-center mt-0 mb-1" onClick={onButtonClick}> 
-        <Card.Img variant="" src= { previewImage } 
+          <Card.Body className="text-center mt-0 mb-1" onClick={onButtonClick} border="primary"> 
+            <Card.Img variant="" src= { previewImage } 
+            
+              style={{width: '120px', height: '130px',cursor:'pointer'}} alt='Socio'/>
+            <input type='file' id='file' accept="image/png, image/gif, image/jpeg, image/jpg, image/svg" ref={inputFile} 
+                    style={{display: 'none'}} multiple onChange={handleFileChange}/>
+            <Card.Text style={{ color: '#0066ff'}}>
+              <strong>{per.apellido || ''} {per.nombre || ''}</strong>
+            </Card.Text>
+            
+
+          </Card.Body>
+          <ListGroup className="list-group-flush mb-0">
+            <ListGroup.Item className="m-0">
+              <p><b style={{ color: '#0066ff'}}>SOCIO:</b> <strong>{per.id || ''}</strong></p>
+              <p><b style={{ color: '#0066ff'}}>DNI:</b> <strong>{per.dni || ''}</strong></p>
+              <p><b style={{ color: '#0066ff'}}>ALTA:</b> <strong>{new Date(per.fchAlt).toLocaleDateString() || ''}</strong></p>
+            </ListGroup.Item>
+          </ListGroup>
+
+          <Table striped className="mb-0">
+            <tbody>
+              <tr>
+                <td >
+                  <Card border="primary">
+                    <Card.Img src="https://www.codigos-qr.com/qr/php/qr_img.php?d=http%3A%2F%2F190.229.66.64%2Fcottucumano%2Fpublic_html%2FCOTonline%2FLoginCard.php%3FSoc%3D1010&s=4&e=m" style={{width: '100%', height: '100px'}} alt='QR'/>
+                  </Card>
+                </td>
+                <td className="text-center">
+                  <Card.Img variant="" src=".././Raya.png" style={{width: '80px', height: '60px'}} alt='Raya'/>
+                  <p style={{color:'#0066ff',fontSize:'10px'}}>Dr. José Luis Raya <br/> Sec. General C.O.T.</p>
+                </td>
+                <td className="text-center">
+                  <Card.Img variant="" src=".././Heredia.png" style={{width: '80px', height: '60px'}} alt='Heredia'/>
+                  <p style={{color:'#0066ff',fontSize:'10px'}}>Dra. Alejandra Heredia <br/> Presidente C.O.T.</p>
+                </td>
+
+              </tr>
+
+            </tbody>
+          </Table>
+          <ListGroup className="list-group-flush mb-0 text-center">
+            <ListGroup.Item action variant={estado==='Activo'?'primary ':'danger'}>
+              <p>Estado:<b> {estado}</b></p>
+            </ListGroup.Item>
+          </ListGroup>
         
-          style={{width: '100px', height: '110px',cursor:'pointer'}} alt='Socio'/>
-        <input type='file' id='file' ref={inputFile} style={{display: 'block'}} multiple
-          onChange={handleFileChange}/>
-        <Card.Text style={{ color: '#0066ff'}}>
-          <strong>{per.apellido || ''} {per.nombre || ''}</strong>
-        </Card.Text>
-        
-        <Button 
-            className="btn btn-primary mt-3" 
-            onClick={handleSubmit}
-            >Upload</Button>
-
-      </Card.Body>
-      <ListGroup className="list-group-flush mb-0">
-        <ListGroup.Item className="m-0">
-          <p><b style={{ color: '#0066ff'}}>SOCIO:</b> <strong>{per.id || ''}</strong></p>
-          <p><b style={{ color: '#0066ff'}}>DNI:</b> <strong>{per.dni || ''}</strong></p>
-          <p><b style={{ color: '#0066ff'}}>ALTA:</b> <strong>{new Date(per.fchAlt).toLocaleDateString() || ''}</strong></p>
-        </ListGroup.Item>
-      </ListGroup>
-
-      <Table striped className="mb-0">
-        <tbody>
-          <tr>
-            <td >
-              <Card border="primary">
-                <Card.Img src="https://www.codigos-qr.com/qr/php/qr_img.php?d=http%3A%2F%2F190.229.66.64%2Fcottucumano%2Fpublic_html%2FCOTonline%2FLoginCard.php%3FSoc%3D1010&s=4&e=m" style={{width: '100%', height: '100px'}} alt='QR'/>
-              </Card>
-            </td>
-            <td className="text-center">
-              <Card.Img variant="" src=".././Raya.png" style={{width: '80px', height: '60px'}} alt='Raya'/>
-              <p style={{color:'#0066ff',fontSize:'10px'}}>Dr. José Luis Raya <br/> Sec. General C.O.T.</p>
-            </td>
-            <td className="text-center">
-              <Card.Img variant="" src=".././Heredia.png" style={{width: '80px', height: '60px'}} alt='Heredia'/>
-              <p style={{color:'#0066ff',fontSize:'10px'}}>Dra. Alejandra Heredia <br/> Presidente C.O.T.</p>
-            </td>
-
-          </tr>
-  
-        </tbody>
-      </Table>
-      <ListGroup className="list-group-flush mb-0 text-center">
-        <ListGroup.Item action variant={estado==='Activo'?'primary ':'danger'}>
-          <p>Estado:<b> {estado}</b></p>
-        </ListGroup.Item>
-      </ListGroup>
+        </>
+      }
 
     </Card>  
    </Card>
